aebc6fd06f7b43bbf44bc5a24f96cb58864b07b9
kim-0617 <kkk5993@naver.com>
2022-10-02 23:37:47 +0900

    22.10.02
    23:37

---
+++ 3.숫자야구/NumberBaseball.jsx
@@ -0,0 +1,4 @@
+const React = require('react');
+const { Component } = React;
+
+module.exports = WordRelay;
---
+++ 3.숫자야구/client.jsx
@@ -0,0 +1,5 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+const NumberBaseball = require('./NumberBaseball');
+
+ReactDom.createRoot(document.querySelector('#root')).render(<NumberBaseball />);
