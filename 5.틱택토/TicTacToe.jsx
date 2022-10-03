632aa01d105beac68d2dea2dbe556367c71188da
kim-0617 <kkk5993@naver.com>
2022-10-03 21:45:01 +0900

    22.10.03
    21:44

---
+++ 5.틱택토/TicTacToe.jsx
@@ -0,0 +1,28 @@
+const React = require('react');
+const { Component } = React;
+
+// class TicTacToe extends Component {
+//     state = {
+//         state : "waiting",
+//         message : "클릭해서 시작하세요!",
+//         result : [],
+//     }
+
+
+//     render() {
+//         return (
+//             <></>
+//         );
+//     }
+// }
+
+const { useState, useRef } = React;
+const TicTacToe = () => {
+
+    return (
+        <>
+        Hello World!
+        </>
+    );
+}
+export default TicTacToe;
