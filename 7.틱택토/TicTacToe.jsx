0e70d9b8daf41ab2c3958a54896c8f595df48ef8
kim-0617 <kkk5993@naver.com>
2022-10-09 02:54:48 +0900

    22.10.09
    2:53

---
+++ 7.틱택토/TicTacToe.jsx
@@ -0,0 +1,27 @@
+const React = require('react');
+const { Component } = React;
+
+class Lotto extends Component {
+    state = {
+
+    }
+
+    render() {
+        return (
+            <>
+            
+            </>
+        );
+    }
+}
+
+const { useState, useRef, useEffect, useMemo, useCallback } = React;
+// const Lotto = () => {
+
+//     return (
+//         <>
+            
+//         </>
+//     );
+// }
+export default Lotto
